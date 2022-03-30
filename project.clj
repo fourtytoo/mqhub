ceed6dc7f501acddc8b86611bb87c6c8a4089360
Walter Pelissero <wcp@book.home.lan>
2022-03-30 16:13:22 +0200

    Add project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject mqhub "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A simple(r) Home IoT Hub based on MQTT written in Clojure"
   :url "http://github.com/fourtytoo/mqhub"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
@@ -7,7 +7,6 @@
                  [org.clojure/core.memoize "1.0.257"]
                  [org.clojure/tools.logging "1.1.0"]
                  [com.taoensso/timbre "5.2.1"]
-                 ;; [spootnik/unilog "0.7.27"]
                  [cprop "0.1.17"]
                  [camel-snake-kebab "0.4.2"]
                  [clojurewerkz/machine_head "1.0.0"]
